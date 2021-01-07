33658eb60e96779801cf1f4169777c865f0f87e0
Allyson Souza <allyson@hastedesign.com.br>
2021-01-06 23:11:55 -0300

    Commit inicial - Criação do repositório e diretório do primeiro exemplo

---
+++ forma-block-static/forma-block-static.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Forma Block Static
+ *
+ * @package           FormaBlockStatic
+ * @author            Forma Haste
+ * @copyright         2021 Forma Haste
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Forma Block Static
+ * Plugin URI:        https://forma.hastedesign.com.br
+ * Description:       A simple static Gutenberg block.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Forma Haste
+ * Author URI:        https://forma.hastedesign.com.br
+ * Text Domain:       forma-block-static
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
